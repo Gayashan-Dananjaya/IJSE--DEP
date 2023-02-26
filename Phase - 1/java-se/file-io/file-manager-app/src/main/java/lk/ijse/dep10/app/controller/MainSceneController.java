b99bb919963c012cb77fedf1e58edd29f4f7511a
Gayashan_Dananjaya <gayashandananjaya3@gmail.com>
2023-02-26 21:44:40 +0530

    File Manager App

--- Phase - 1/java-se/file-io/file-manager-app/src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
+++ Phase - 1/java-se/file-io/file-manager-app/src/main/java/lk/ijse/dep10/app/controller/MainSceneController.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep10.app.controller;
 
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -39,7 +41,12 @@ public class MainSceneController {
     private TextField txtTo;
     File fileFrom;
     File fileTo;
+    double readSize = 0;
+    double writeCount = 0;
+    boolean moveRec = false;
 
+    public void initialize() {
+    }
     @FXML
     void btnFromOnAction(ActionEvent event) {
 //        JFileChooser chooser = new JFileChooser();
@@ -47,11 +54,11 @@ public class MainSceneController {
 //        chooser.showOpenDialog(null);
 //        fileFrom = chooser.getSelectedFile();
 
-//        DirectoryChooser dirChooser = new DirectoryChooser();
-//        fileFrom = dirChooser.showDialog(btnFrom.getScene().getWindow());
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        fileFrom = dirChooser.showDialog(btnFrom.getScene().getWindow());
 
-        FileChooser fileChooser = new FileChooser();
-        fileFrom = fileChooser.showOpenDialog(btnFrom.getScene().getWindow());
+//        FileChooser fileChooser = new FileChooser();
+//        fileFrom = fileChooser.showOpenDialog(btnFrom.getScene().getWindow());
 
         if (fileFrom == null) {
             txtFrom.setText("");
@@ -76,63 +83,193 @@ public class MainSceneController {
     void btnCopyOnAction(ActionEvent event) {
         if (fileFrom == null || fileTo == null) {
             return;
-        }
-
-        if (fileFrom.isFile()) {
+        } else if (fileFrom.isFile()) {
             fileTo = new File(fileTo, fileFrom.getName());
-
-            Task<Void> task = new Task<>() {
-                @Override
-                protected Void call() throws Exception {
-                    try {
-                        double readSize = fileFrom.length();
-                        double writeSize = 0;
-                        FileInputStream fis = new FileInputStream(fileFrom);
-                        FileOutputStream fos = new FileOutputStream(fileTo,true);
-
-
-
-                        while (true) {
-                            byte[] bytes = new byte[1024];
-                            int read = fis.read(bytes);
-
-                            if (read == -1) {
-                                break;
-                            } else {
-                                fos.write(bytes,0,read);
-                                writeSize += read;
-                                updateProgress(writeSize/readSize,1);
-                            }
-                        }
-
-                        System.out.println("Write finished");
-                        fis.close();
-                        fos.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    return null;
-                }
-            };
-
-            new Thread(task).start();
-            prg.progressProperty().bind(task.progressProperty());
-
-
-
+            readSize = fileFrom.length();
+            copySingleFile(fileFrom, fileTo);
+            System.out.println("Write completed");
+        } else if (fileFrom.isDirectory()){
+            getSize(fileFrom);
+            File newFile = new File(fileTo, fileFrom.getName());
+            newFile.mkdir();
+            copyRecursion(fileFrom, newFile);
+            System.out.println("Write completed");
         }
+
+
+
+
 
     }
 
     @FXML
     void btnMoveOnAction(ActionEvent event) {
-
+        if (fileFrom == null || fileTo == null) {
+            return;
+        } else if (fileFrom.isFile()) {
+            moveRec = false;
+            fileTo = new File(fileTo, fileFrom.getName());
+            readSize = fileFrom.length();
+            moveSingleFile(fileFrom, fileTo);
+            System.out.println("Move completed");
+        } else if (fileFrom.isDirectory()){
+            moveRec = true;
+            getSize(fileFrom);
+            File newFile = new File(fileTo, fileFrom.getName());
+            newFile.mkdir();
+            moveRecursion(fileFrom, newFile);
+//            fileFrom.delete();
+            System.out.println("Move completed");
+        }
     }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
+        if (fileFrom == null) {
+            return;
+        } else if (fileFrom.isFile()) {
+            fileFrom.delete();
+            System.out.println("Delete completed");
+        } else if (fileFrom.isDirectory()) {
+            deleteRecursion(fileFrom);
+            fileFrom.delete();
+            System.out.println("Delete completed");
+        }
+    }
 
+    public void copySingleFile(File start, File destination) {
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                try {
+                    FileInputStream fis = new FileInputStream(start);
+                    FileOutputStream fos = new FileOutputStream(destination,true);
+
+                    while (true) {
+                        byte[] bytes = new byte[1024];
+                        int read = fis.read(bytes);
+
+
+                        if (read == -1) {
+                            break;
+                        } else {
+                            fos.write(bytes,0,read);
+                            writeCount += read;
+                            updateProgress(writeCount/readSize,2);
+//                            System.out.println(writeCount/readSize);
+                        }
+                    }
+
+                    fis.close();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+
+        new Thread(task).start();
+//        prg.progressProperty().unbind();
+        prg.progressProperty().bind(task.progressProperty());
+    }
+
+    public void copyRecursion(File copyFrom, File copyTo) {
+        File[] filesList = copyFrom.listFiles();
+//        if (filesList.length == 0) {
+//            return;
+//        }
+        for (File file : filesList) {
+            if (file.isFile()) {
+                copySingleFile(file, new File(copyTo, file.getName()));
+            } else if (file.isDirectory()) {
+                File newFile = new File(copyTo, file.getName());
+                newFile.mkdir();
+                copyRecursion(file, newFile);
+            }
+        }
+    }
+
+    public void getSize(File file) {
+        File[] filesArray = file.listFiles();
+        for (File selectedFile : filesArray) {
+            if (selectedFile.isFile()) {
+                readSize += selectedFile.length();
+            } else if (selectedFile.isDirectory()) {
+                getSize(selectedFile);
+            }
+        }
+    }
+
+    public void deleteRecursion(File directory) {
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                file.delete();
+            } else if (file.isDirectory()) {
+                deleteRecursion(file);
+                file.delete();
+            }
+        }
+    }
+
+    public void moveSingleFile(File start, File destination) {
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                try {
+                    FileInputStream fis = new FileInputStream(start);
+                    FileOutputStream fos = new FileOutputStream(destination,true);
+
+                    while (true) {
+                        byte[] bytes = new byte[1024];
+                        int read = fis.read(bytes);
+
+
+                        if (read == -1) {
+                            break;
+                        } else {
+                            fos.write(bytes,0,read);
+                            writeCount += read;
+                            updateProgress(writeCount/readSize,2);
+//                            System.out.println(writeCount/readSize);
+                        }
+                    }
+
+                    fis.close();
+                    fos.close();
+
+                    start.delete();
+                    if (moveRec) {
+                        start.getParentFile().delete();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+
+        new Thread(task).start();
+//        prg.progressProperty().unbind();
+        prg.progressProperty().bind(task.progressProperty());
+    }
+
+    public void moveRecursion(File copyFrom, File copyTo) {
+        File[] filesList = copyFrom.listFiles();
+//        if (filesList.length == 0) {
+//            return;
+//        }
+        for (File file : filesList) {
+            if (file.isFile()) {
+                moveSingleFile(file, new File(copyTo, file.getName()));
+            } else if (file.isDirectory()) {
+                File newFile = new File(copyTo, file.getName());
+                newFile.mkdir();
+                moveRecursion(file, newFile);
+//                file.delete();
+            }
+        }
     }
 
 }
